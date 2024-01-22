61b68ce407b01aedd403ba6efa45e0af64ad3779
Kanagamani <Kanagamani.T@cognizant.com>
2024-01-22 19:24:54 +0530

    code added

---
+++ assignment28/assignment28/src/test/java/com/example/assignment28/Assignment28ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.assignment28;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment28ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
